70a12002a1c9d868daa75f0f2eb043055b766efa
Антон <lopatuxin.a@gmail.com>
2023-01-30 16:07:45 +0300

    2.1. Преобразование даты [#289476 #386364]

--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -21,6 +21,6 @@ class HabrCareerDateTimeParserTest {
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         String date = "2023-01-29T20:27:33+03:00";
         LocalDateTime expected = LocalDateTime.of(2023, 03, 29, 20, 27, 33);
-        assertThat(expected.isEqual(parser.parse(date))).isFalse();
+        assertThat(expected).isNotEqualTo(parser.parse(date));
     }
 }
