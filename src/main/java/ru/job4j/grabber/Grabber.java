22fba4146c8efaa06bed9e33afd0100850aef9d1
Антон <lopatuxin.a@gmail.com>
2023-01-31 20:24:05 +0300

    6. Grabber. [#289477]

---
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -0,0 +1,74 @@
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private final Properties config = new Properties();
+
+    public Store store() {
+        return new PsqlStore(config);
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void cfg() throws IOException {
+        try (InputStream in = Grabber.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            config.load(in);
+        }
+    }
+
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            String pageLink = (String) map.get("link");
+            List<Post> posts = new ArrayList<>(parse.list(pageLink));
+            posts.forEach(store::save);
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Grabber grab = new Grabber();
+        grab.cfg();
+        Scheduler scheduler = grab.scheduler();
+        Store store = grab.store();
+        grab.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+    }
+}
