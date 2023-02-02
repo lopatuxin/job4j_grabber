62020292ee73f41e0414214f18d794bd7f3f86d9
Антон <lopatuxin.a@gmail.com>
2023-02-02 21:28:49 +0300

    2. SQL Select by company [#2760]

--- src/main/resources/scripts/company_person.sql
+++ src/main/resources/scripts/company_person.sql
@@ -13,3 +13,27 @@ CREATE TABLE person
     CONSTRAINT person_pkey PRIMARY KEY (id)
 );
 
+insert into company
+values(1,'Google'), (2, 'BMW'), (3, 'LADA'), (4, 'LG'), (5, 'VAG');
+insert into person
+values (1, 'Anton', 1), (2, 'Bob', 1), (3, 'Bill', 2), (4, 'Rob', 2), (5, 'Vas', 3), (6, 'Aleks', 4);
+
+--1
+select name from person
+where id = 5 and company_id is null;
+
+--2
+select person.name, company.name from person
+join company on company_id = company.id;
+
+--3
+select c.name, count(p.name) as Количество
+from company c
+join person p on p.company_id = c.id
+group by c.name
+having count(company_id) = (
+	select max(maximum)
+	from (select company_id, count(company_id) as maximum
+	from person
+	group by company_id) as q1);
+
