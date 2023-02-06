2d4b3f5f04265c0dacfb9c8b6fa545b5f10f3136
Антон <lopatuxin.a@gmail.com>
2023-02-06 16:42:18 +0300

    4. Найти утечку памяти. [#504882 #388548]

--- src/main/java/ru/job4j/gc/leak/CommentGenerator.java
+++ src/main/java/ru/job4j/gc/leak/CommentGenerator.java
@@ -9,8 +9,8 @@ public class CommentGenerator implements Generate {
     public static final String PATH_PHRASES = "src/main/java/ru/job4j/gc/leak/files/phrases.txt";
 
     public static final String SEPARATOR = System.lineSeparator();
-    private final List<Comment> comments = new ArrayList<>();
     public static final Integer COUNT = 50;
+    private final List<Comment> comments = new ArrayList<>();
     private List<String> phrases;
     private final UserGenerator userGenerator;
     private final Random random;
