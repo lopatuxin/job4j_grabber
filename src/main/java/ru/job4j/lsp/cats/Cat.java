0fad599656b8b07a32e6478eb1b7799948e5f9f1
Антон <lopatuxin.a@gmail.com>
2023-02-10 21:22:03 +0300

    0. Принцип подстановки Лисков. [#4915]

---
+++ src/main/java/ru/job4j/lsp/cats/Cat.java
@@ -0,0 +1,8 @@
+package ru.job4j.lsp.cats;
+
+public class Cat {
+
+    public void meow() {
+        System.out.println("Мяу");
+    }
+}
---
+++ src/main/java/ru/job4j/lsp/cats/Lion.java
@@ -0,0 +1,8 @@
+package ru.job4j.lsp.cats;
+
+public class Lion extends Cat {
+
+    public void roar(){
+        System.out.println("Рёв");
+    }
+}
