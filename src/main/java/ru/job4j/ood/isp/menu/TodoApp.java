672cd58149f0f49b2344c9094c59ac2cde0f321a
Антон <lopatuxin.a@gmail.com>
2023-02-19 19:36:10 +0300

    1. Создать меню. [#4748 #391465]

--- src/main/java/ru/job4j/ood/isp/menu/TodoApp.java
+++ src/main/java/ru/job4j/ood/isp/menu/TodoApp.java
@@ -3,15 +3,19 @@ package ru.job4j.ood.isp.menu;
 import java.util.Scanner;
 
 public class TodoApp {
-    private static final String ADD_ROOT = "1.Добавить элемент в корень меню";
-    private static final String ADD_CHILD = "2.Добавить элемент к родительскому элементу";
-    private static final String ACTION = "3.Вызвать действие, привязанное к пункту меню";
-    private static final String PRINT_MENU = "4.Вывести меню в консоль";
-    private static final String EXIT = "Для выхода - любая другая цифра";
-    private static final ActionDelegate DEFAULT_ACTION = () -> System.out.println("Some action");
-    private static final String MENU = String.join(System.lineSeparator(),
-            ADD_ROOT, ADD_CHILD, ACTION, PRINT_MENU, EXIT + System.lineSeparator());
+    private static final String MENU = """
+            1.Добавить элемент в корень меню.
+            2.Добавить элемент к родительскому элементу.
+            3.Вывод приветствия.
+            4.Печать меню.
+            Для выхода - любая другая цифра.
+            """;
+    private static final int ADD_ROOT = 1;
+    private static final int ADD_CHILD = 2;
+    private static final int SOME_ACTION = 3;
+    private static final int PRINT_MENU = 4;
     private static final String ASK = "Выберите пункт меню ";
+    public static final ActionDelegate DEFAULT_ACTION = () -> System.out.println("Hello!");
 
     public static void main(String[] args) {
         Menu menu = new SimpleMenu();
@@ -24,22 +28,22 @@ public class TodoApp {
             System.out.println(ASK);
             int choice = Integer.parseInt(sc.nextLine());
             switch (choice) {
-                case 1:
+                case ADD_ROOT:
                     System.out.println("Укажите имя элемента: ");
                     name = sc.nextLine();
                     menu.add(Menu.ROOT, name, DEFAULT_ACTION);
                     break;
-                case 2:
+                case ADD_CHILD:
                     System.out.println("Укажите имя родительского элемента: ");
                     name = sc.nextLine();
                     System.out.println("Укажите имя добавляемого элемента: ");
                     String child = sc.nextLine();
                     menu.add(name, child, DEFAULT_ACTION);
                     break;
-                case 3:
+                case SOME_ACTION:
                     DEFAULT_ACTION.delegate();
                     break;
-                case 4:
+                case PRINT_MENU:
                     printer.print(menu);
                     break;
                 default:
