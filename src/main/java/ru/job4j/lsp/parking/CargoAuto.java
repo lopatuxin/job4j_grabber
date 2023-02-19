2f516987b54660dcb0ab57b177bb6423c7d0befe
Антон <lopatuxin.a@gmail.com>
2023-02-19 19:25:47 +0300

    2. Парковка машин [#853 #391141]

--- src/main/java/ru/job4j/lsp/parking/CargoAuto.java
+++ src/main/java/ru/job4j/lsp/parking/CargoAuto.java
@@ -3,7 +3,7 @@ package ru.job4j.lsp.parking;
 public class CargoAuto extends Auto {
     public CargoAuto(int size, String name) {
         super(size, name);
-        if (size <= 1) {
+        if (size <= PassengerAuto.SIZE) {
             throw new IllegalArgumentException("Size must be more than 1");
         }
     }
--- src/main/java/ru/job4j/lsp/parking/PassengerAuto.java
+++ src/main/java/ru/job4j/lsp/parking/PassengerAuto.java
@@ -2,7 +2,7 @@ package ru.job4j.lsp.parking;
 
 public class PassengerAuto extends Auto {
 
-    private static final int SIZE = 1;
+    public static final int SIZE = 1;
     public PassengerAuto(String name) {
         super(SIZE, name);
     }
