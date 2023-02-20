adb81138b593cb6d09bf0b7928356a2a5cfc072e
Антон <lopatuxin.a@gmail.com>
2023-02-20 19:48:23 +0300

    2. Парковка машин [#853 #391141]

--- src/main/java/ru/job4j/lsp/parking/CarPark.java
+++ src/main/java/ru/job4j/lsp/parking/CarPark.java
@@ -4,33 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarPark implements Parking {
-    private final int CARGO_COUNT;
+    private final int cargoCount;
     private int cargoCounter = 0;
-    private final int PASSENGER_COUNT;
+    private final int passengerCount;
     private int passengerCounter = 0;
     private List<Auto> autos;
 
-    public CarPark(int CARGO_COUNT, int PASSENGER_COUNT) {
-        this.CARGO_COUNT = CARGO_COUNT;
-        this.PASSENGER_COUNT = PASSENGER_COUNT;
+    public CarPark(int cargoCount, int passengerCount) {
+        this.cargoCount = cargoCount;
+        this.passengerCount = passengerCount;
         this.autos = new ArrayList<>();
     }
 
     @Override
     public boolean add(Auto auto) {
-        if (auto.getSize() > 1 && cargoCounter < CARGO_COUNT) {
+        if (auto.getSize() > 1 && cargoCounter < cargoCount) {
             autos.add(auto);
             cargoCounter++;
             return true;
         }
-        if (auto.getSize() == 1 && passengerCounter < PASSENGER_COUNT) {
+        if (auto.getSize() == 1 && passengerCounter < passengerCount) {
             autos.add(auto);
             passengerCounter++;
             return true;
         }
         if (auto.getSize() > 1
-                && cargoCounter >= CARGO_COUNT
-                && PASSENGER_COUNT - passengerCounter >= auto.getSize()) {
+                && cargoCounter >= cargoCount
+                && passengerCount - passengerCounter >= auto.getSize()) {
             autos.add(auto);
             passengerCounter = passengerCounter + auto.getSize();
             return true;
